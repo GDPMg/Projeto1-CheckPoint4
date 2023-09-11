c39b93869954f2a222b9414e3c62b473ec407aef
Matheus <mabrisqui@gmail.com>
2023-09-11 17:58:46 -0400

    Fazendo o codigo do sobre

---
+++ src/componentes/Sobre.jsx
@@ -0,0 +1,44 @@
+import {} from 'react'
+
+
+function Sobre() {
+  
+
+  return (
+    <>
+    <img src="https://picsum.photos/200/300" alt="imagem" />
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus qui, nemo tempore quidem animi nostrum mollitia voluptas neque sint maxime fugit excepturi ab eaque, eum, praesentium inventore veritatis officiis commodi.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br> Sapiente ratione iusto tempora quis illum illo aperiam. Quasi porro commodi assumenda quisquam, libero nisi eligendi ad, tempora magnam tempore fugit. Deserunt.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eius veniam pariatur deleniti, minus laudantium facilis soluta sit nam quaerat expedita vero inventore voluptas vitae amet consectetur, tempora debitis quae?
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis magni ipsum corporis quis fuga totam error libero,<br></br> ex architecto, sequi possimus natus, voluptas vitae ratione ea. Alias pariatur laborum est!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rem neque provident nostrum minus officia quaerat reiciendis molestiae ad nesciunt pariatur, laboriosam necessitatibus dolorum, labore magni quis molestias, facere eveniet.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima pariatur iste laboriosam itaque temporibus at quos officia, ea sequi accusantium, reprehenderit ratione recusandae, voluptates saepe unde eaque veritatis rerum ad.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque unde iusto reprehenderit delectus illo omnis nobis culpa, explicabo veniam vero magnam fugit accusantium, molestiae provident exercitationem iure soluta atque laudantium!
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam accusamus molestiae repe<br></br>llendus temporibus, iusto facere? Maxime cum, dolorem nihil dolore voluptates fugit ea error quaerat voluptatem nulla, sapiente iste. Repellat!
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam esse, repellat cum labore magnam ut tempora aut sed suscipit nihil optio rerum! Itaque quas nesciunt alias, commodi illum earum aperiam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique esse et molestiae illo incidunt aut commodi voluptatum voluptatibus, soluta enim modi reiciendis ab itaque odio beatae rem labore eligendi sint.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quod quos dignissimos sint perspiciatis molestiae recusandae nihil porro, officiis omnis hic similique, placeat quam adipisci. Culpa dignissimos explicabo asperiores tempore.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere alias ab minus voluptate vero debitis corrupti minima quisquam nobis, ipsum dignissimos asperiores id ad temporibus libero quos impedit! Eius, fuga.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta quasi, molestiae quae sit quia vel nostrum ea dolore. In est fugit odit perferendis laboriosam libero ad! Fuga delectus et sed.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt sit dolores rem quo quia molestiae ab neque laborum eligendi mollitia deserunt sequi assumenda, itaque ex officiis. Totam quis consectetur magni.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus qui, nemo tempore quidem animi nostrum mollitia voluptas neque sint maxime fugit excepturi ab eaque, eum, praesentium inventore veritatis officiis commodi.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br> Sapiente ratione iusto tempora quis illum illo aperiam. Quasi porro commodi assumenda quisquam, libero nisi eligendi ad, tempora magnam tempore fugit. Deserunt.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eius veniam pariatur deleniti, minus laudantium facilis soluta sit nam quaerat expedita vero inventore voluptas vitae amet consectetur, tempora debitis quae?
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis magni ipsum corporis quis fuga totam error libero,<br></br> ex architecto, sequi possimus natus, voluptas vitae ratione ea. Alias pariatur laborum est!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rem neque provident nostrum minus officia quaerat reiciendis molestiae ad nesciunt pariatur, laboriosam necessitatibus dolorum, labore magni quis molestias, facere eveniet.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima pariatur iste laboriosam itaque temporibus at quos officia, ea sequi accusantium, reprehenderit ratione recusandae, voluptates saepe unde eaque veritatis rerum ad.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque unde iusto reprehenderit delectus illo omnis nobis culpa, explicabo veniam vero magnam fugit accusantium, molestiae provident exercitationem iure soluta atque laudantium!
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam accusamus molestiae repe<br></br>llendus temporibus, iusto facere? Maxime cum, dolorem nihil dolore voluptates fugit ea error quaerat voluptatem nulla, sapiente iste. Repellat!
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam esse, repellat cum labore magnam ut tempora aut sed suscipit nihil optio rerum! Itaque quas nesciunt alias, commodi illum earum aperiam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique esse et molestiae illo incidunt aut commodi voluptatum voluptatibus, soluta enim modi reiciendis ab itaque odio beatae rem labore eligendi sint.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quod quos dignissimos sint perspiciatis molestiae recusandae nihil porro, officiis omnis hic similique, placeat quam adipisci. Culpa dignissimos explicabo asperiores tempore.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere alias ab minus voluptate vero debitis corrupti minima quisquam nobis, ipsum dignissimos asperiores id ad temporibus libero quos impedit! Eius, fuga.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta quasi, molestiae quae sit quia vel nostrum ea dolore. In est fugit odit perferendis laboriosam libero ad! Fuga delectus et sed.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt sit dolores rem quo quia molestiae ab neque laborum eligendi mollitia deserunt sequi assumenda, itaque ex officiis. Totam quis consectetur magni.
+
+    </p>
+    </>
+  )
+}
+
+export default Sobre
