f8dbe4d0650ac15ff047333f6de3dceadfc28cb9
Gustavo <gugabrisqui@gmail.com>
2023-09-11 17:55:52 -0400

    Fazendo codigo do produto

---
+++ src/componentes/Produto.jsx
@@ -0,0 +1,34 @@
+import {} from 'react'
+
+function Produtos() {
+ 
+
+
+
+  return (
+    <>
+    <img src="https://picsum.photos/200/300" alt="imagem" />
+    <figcaption>
+        <p>Titulo da imagem 1</p>
+    </figcaption>
+
+    <img src="https://picsum.photos/200/300" alt="imagem" />
+    <figcaption>
+        <p>Titulo da imagem 2</p>
+    </figcaption>
+
+    <img src="https://picsum.photos/200/300" alt="imagem" />
+    <figcaption>
+        <p>Titulo da imagem 3</p>
+    </figcaption>
+
+    <img src="https://picsum.photos/200/300" alt="imagem" />
+    <figcaption>
+        <p>Titulo da imagem 4</p>
+    </figcaption>
+
+    </>
+  )
+}
+
+export default Produtos
